46b45bd285d8f1a50a6abb6527812dab4fd1afb9
killswitch <166486190+agongadavid@users.noreply.github.com>
2025-03-30 17:25:41 +0300

    completed animal and subclass dog and cat

---
+++ Animal.java
@@ -0,0 +1,10 @@
+public class Animal{
+    public void makeSound() {
+        System.out.println("The animal sound is:");
+    }
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound();
+    }
+}
